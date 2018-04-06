5a5f3ab21492d5246f7c0f02ff801fa7852aeb12
gesucca <simcip.ssdev@gmail.com>
2018-04-06 15:26:13 +0200

    Rischy refactor, but badly neeeded

--- prepr/prepr.py
+++ prepr/prepr.py
@@ -5,10 +5,13 @@ from pymongo import MongoClient
 
 from cleanings import Cleaner
 from cleanings import QSET_OLD, QSET_GEN
+
 from aggregs import Aggregator
+
 from discretize import discretize
 from discretize import VAL_SCORE, STD_DEV, MARKS, ZERO_TO_HUND
-from merge import merge_teach, the_big_merge
+
+from merge import merge_teach, gen_minable_01
 
 
 def main(scheme):
@@ -18,20 +21,24 @@ def main(scheme):
     _clean(scheme, teval)
     _aggreg(scheme.rawStudentsPr1013, sprod)
 
-    # aaaargh
-    discretize(teval, scheme.create_collection("teval_discrete"),
+    teval_d = scheme.create_collection("teval_discrete")
+    sprod_d = scheme.create_collection("sprod_discrete")
+    discretize(teval, teval_d,
                ['Media', 'Deviazione standard', 'N', 'P<6', 'P>=6'],
                [VAL_SCORE, STD_DEV, ZERO_TO_HUND, ZERO_TO_HUND, ZERO_TO_HUND],
-               True)
+               True
+              )
 
-    discretize(sprod, scheme.create_collection("sprod_discrete"),
+    discretize(sprod, sprod_d,
                ['Media', 'Deviazione standard', 'N', 'P<24', 'P>=24'],
                [MARKS, STD_DEV, ZERO_TO_HUND, ZERO_TO_HUND, ZERO_TO_HUND],
-               True)
+               True
+              )
 
-    merge_teach(scheme.teval_discrete)
-    the_big_merge(scheme.teval_discrete, scheme.sprod_discrete,
-                  scheme.create_collection('minable_01'), True)
+    merge_teach(scheme.teval_discrete, ['Dataset Provenienza', 'Insegnamento'])
+
+    gen_minable_01(scheme.teval_discrete, scheme.sprod_discrete,
+                   scheme.create_collection('minable_01'), True)
 
 
 def _clean(scheme, dest):
