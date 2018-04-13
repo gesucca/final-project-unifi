a19da6e091fc7fd9c67864c25f8bb317b2241d2f
gesucca <simcip.ssdev@gmail.com>
2018-04-13 12:36:49 +0200

    Big stuff this time

--- prepr/discretize.py
+++ prepr/discretize.py
@@ -15,13 +15,6 @@ class _DiscRange:
         return self._label
 
 
-VAL_SCORE = {_DiscRange(0, 4), _DiscRange(4, 6), _DiscRange(6, 8), _DiscRange(8, 10)}
-STD_DEV = {_DiscRange(0, 1.5), _DiscRange(1.5, 3), _DiscRange(3, 5), _DiscRange(5, 8)}
-MARKS = {_DiscRange(18, 22), _DiscRange(22, 25), _DiscRange(25, 28), _DiscRange(28, 31)}
-ZERO_TO_HUND = {_DiscRange(0, 20), _DiscRange(20, 40), _DiscRange(40, 60),
-                _DiscRange(60, 80), _DiscRange(80, 100), _DiscRange(100, 120)}
-
-
 def _discretize(value, ranges):
     if value == 'n.c.' or value == '<5':
         return value.upper()
