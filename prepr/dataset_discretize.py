c32cd9853f518b9ae4d9a8687eb04005e67fd695
gesucca <simcip.ssdev@gmail.com>
2018-05-07 14:51:42 +0200

    Fixed export and discretization

--- prepr/dataset_discretize.py
+++ prepr/dataset_discretize.py
@@ -9,8 +9,8 @@ minable = scheme['minable']
 minable_discretized = scheme.create_collection("minable_discretized")
 
 discretize.discretize(minable, minable_discretized,
-                      ['Media', 'Std Dev', 'N', 'P>=6', 'P>=24',
-                       'Voto Medio', 'Ritardo Medio', 'P>=1sem'],
+                      ['Val [media pesata]', 'Std Dev [media pesata]', 'N [istanze]', '>= 6', '>= 24',
+                       'Voto [media]', 'Ritardo [semestre, media]', '>=1sem'],
                       [pp.VAL_SCORE, pp.STD_DEV, pp.STUDENTS, pp.PERCENT, pp.PERCENT,
                        pp.MARKS, pp.YEARS, pp.PERCENT],
                       False)
--- prepr/export.sh
+++ prepr/export.sh
@@ -6,7 +6,6 @@ sed -i -e 's/"//g' _exp_fields.txt
 sed -i -e 's/,//g' _exp_fields.txt
 sed -i -e '1,16d' _exp_fields.txt # mongo client echoed stuff
 sed -i -e 's/_id//g' _exp_fields.txt
-sed -i -e 's/]//g' _exp_fields.txt
 sed -i -e 's/bye//g' _exp_fields.txt
 sed -i -e '/^\s*$/d' _exp_fields.txt # empty lines
 
