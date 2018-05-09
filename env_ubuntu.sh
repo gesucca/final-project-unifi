3026c8d9a8f0682e7b8c4a7ac12432fccdf04296
gesucca <simcip.ssdev@gmail.com>
2018-05-09 13:31:58 +0200

    Started to try thesis related stuff

--- env_ubuntu.sh
+++ env_ubuntu.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+# preprocessing
 sudo apt install mongodb mongo-tools -y
 pip3 install pymongo
+
+# thesis
+sudo apt install texlive texlive-science texlive-lang-italian texlive-latex-extra texlive-fonts-extra -y
+
